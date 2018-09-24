de5d30384e0c12915f26f3504addcc337e7cf2b1
Konstantin Onufrijev <konstantin.onufrijevs@ideaportriga.lv>
2018-09-24 22:24:56 +0300

    trie tests

--- test/test-key-trie.ts
+++ test/test-key-trie.ts
@@ -55,29 +55,51 @@ describe("key trie", function () {
     it("works with deep tree", function () {
 
         const trie = new KeyTrie();
-        trie.addKey({ key: "abc", keyType: "", size: 100 });
-        trie.addKey({ key: "ab", keyType: "", size: 100 });
-        trie.addKey({ key: "a", keyType: "", size: 150 });
+        trie.addKey({ key: "d", keyType: "", size: 150 });
+        trie.addKey({ key: "abdc", keyType: "", size: 100 });
+        trie.addKey({ key: "abdaaaabbbbbbb", keyType: "", size: 46 });
         trie.addKey({ key: "abd", keyType: "", size: 80 });
         trie.addKey({ key: "abde", keyType: "", size: 180 });
+        trie.addKey({ key: "abdaaaaa", keyType: "", size: 60 });
+        trie.addKey({ key: "abdaaaab", keyType: "", size: 180 });
 
         const resultNodes = trie.getCompactTree();
-        const total = 100 + 100 + 150 + 80 + 180;
+        const total = 150 + 100 + 46 + 80 + 180 + 60 + 180;
 
         expect(resultNodes).toEqual([
             {
-                prefix: "ab",
-                memory: "100%",
+                prefix: "abd",
+                memory: getPercent((total - 150) / total),
                 children: [
                     {
-                        prefix: "abde",
-                        memory: getPercent((80 + 180) / total)
+                        prefix: "abdaaaa",
+                        memory: getPercent((60 + 180 + 46) / total),
+                        children: [
+                            {
+                                prefix: "abdaaaab",
+                                memory: getPercent((180 + 46) / total),
+                                children: [
+                                    {
+                                        prefix: "abdaaaabbbbbbb",
+                                        memory: getPercent(46 / total)
+                                    }
+                                ]
+                            },
+                            { prefix: "abdaaaaa", memory: getPercent(60 / total) }
+                        ]
                     },
                     {
-                        prefix: "abc",
+                        prefix: "abde",
+                        memory: getPercent(180 / total)
+                    },
+                    {
+                        prefix: "abdc",
                         memory: getPercent(100 / total)
                     }
                 ]
+            }, {
+                prefix: "d",
+                memory: getPercent(150 / total)
             }
         ]);
     });
